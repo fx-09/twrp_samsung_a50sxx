3f813e363dbfb63d6314e7f9f84a00ea81b9055d
halcyon441-png <63539209+halcyon441-png@users.noreply.github.com>
2020-05-04 19:51:15 +0700

    update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,3 +1,20 @@
+
+#
+# Copyright (C) 2019 geiti94
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 DEVICE_PATH := device/samsung/a50sxx
 
 # Architecture
@@ -5,7 +22,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a73
+TARGET_CPU_VARIANT := cortex-a53
 TARGET_CPU_SMP := true
 
 TARGET_2ND_ARCH := arm
@@ -21,26 +38,18 @@ ENABLE_SCHEDBOOST := true
 ALLOW_MISSING_DEPENDENCIES=true
 
 # Bootloader
-BOARD_VENDOR := samsung
-TARGET_SOC := exynos9610 
-TARGET_BOOTLOADER_BOARD_NAME := universal9610
+TARGET_BOOTLOADER_BOARD_NAME := universal9611
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
 
 # Kernel
-#TARGET_KERNEL_SOURCE := kernel/samsung/exynos9610
-#TARGET_KERNEL_CONFIG := exynos9610_a50sxx_defconfig
-
-# Image
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/recoverydtbo
-
-BOARD_KERNEL_IMAGE_NAME := Image
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
-
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPSE30A001RU
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/recoverydtbo
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
+
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPRL05B001RU
 
 # Platform
 TARGET_BOARD_PLATFORM := exynos5
@@ -64,13 +73,13 @@ TARGET_COPY_OUT_VENDOR := vendor
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
 
-# TWRP
-TW_USE_TOOLBOX := true
+# TWRP specific build flags
+RECOVERY_VARIANT := twrp
 TW_THEME := portrait_hdpi
 RECOVERY_SDCARD_ON_DATA := true
 TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
-TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 100
+TW_MAX_BRIGHTNESS := 25500
+TW_DEFAULT_BRIGHTNESS := 16300
 TW_Y_OFFSET := 80
 TW_H_OFFSET := -80
 TW_NO_REBOOT_BOOTLOADER := true
@@ -79,4 +88,4 @@ TW_INCLUDE_NTFS_3G := true
 TW_EXTRA_LANGUAGES := true
 TW_USE_NEW_MINADBD := true
 BOARD_SUPPRESS_SECURE_ERASE := true
-PLATFORM_SECURITY_PATCH := 2020-01-01
+PLATFORM_SECURITY_PATCH := 2099-12-31
--- omni_a50sxx.mk
+++ omni_a50sxx.mk
@@ -1,3 +1,19 @@
+#
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # Release name
 PRODUCT_RELEASE_NAME := a50sxx
 
@@ -13,8 +29,3 @@ PRODUCT_NAME := omni_a50sxx
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy A50s
 PRODUCT_MANUFACTURER := Samsung
-
-# Display Note: Place these flags here only. Getting error (TW_THEME_WARNING_MSG) if placed in BoardConfig.mk
-TARGET_SCREEN_HEIGHT := 2340
-TARGET_SCREEN_WIDTH := 1080
-TW_THEME := portrait_hdpi
