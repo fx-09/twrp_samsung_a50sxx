ec032fc3123e18eae51ef91aa1f36a27851c42d1
halcyon441-png <63539209+halcyon441-png@users.noreply.github.com>
2020-05-05 14:24:17 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,6 @@
 
 #
-# Copyright (C) 2019 geiti94
+# Copyright (C) 2020 halcyon441
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
